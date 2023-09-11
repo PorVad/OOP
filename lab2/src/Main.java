f3a96693249b34411119a8a2eb519213586e3a5f
PorVad <143946767+MDPLofficial@users.noreply.github.com>
2023-09-11 18:01:52 -0400

    lab2 is done

--- lab2/src/Main.java
+++ lab2/src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        Book[] books = new Book[3];
+        Book[] books = new Book[2];
 
         books[0] = new Book(1, "Clean Code", "Martin", "Book", 2017, 186, 690);
         books[1] = new Book(2, "Robert", "Second World War", "Book", 2014, 250, 340);
-        books[2] = new Book(3, "Jhon", "Population", "Book", 2007, 153, 299);
+//        books[2] = new Book(3, "Jhon", "Population", "Book", 2007, 153, 299);
 
         System.out.println("All books:\n");
         printBooks(books);
