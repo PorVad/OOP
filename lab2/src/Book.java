7045c8cedd18f6a045e8cbd5be2fcc1e271676e1
PorVad <143946767+MDPLofficial@users.noreply.github.com>
2023-09-14 10:21:31 -0400

    lab2 done

--- lab2/src/Book.java
+++ lab2/src/Book.java
@@ -7,35 +7,91 @@ public class Book {
     private int pages;
     private int price;
 
-     public Book(int idBook, String name, String author, String company, int year, int pages, int price) {
-         this.idBook = idBook;
-         this.name = name;
-         this.author = author;
-         this.company = company;
-         this.year = year;
-         this.pages = pages;
-         this.price = price;
-     }
+    public Book(int idBook, String name, String author, String company, int year, int pages, int price) {
+        this.idBook = idBook;
+        this.name = name;
+        this.author = author;
+        this.company = company;
+        this.year = year;
+        this.pages = pages;
+        this.price = price;
+    }
 
-     public void setValue(int idBook, String name, String author, String company, int year, int pages, int price) {
-         this.idBook = idBook;
-         this.name = name;
-         this.author = author;
-         this.company = company;
-         this.year = year;
-         this.pages = pages;
-         this.price = price;
-     }
+    public void setIdBook(int idBook) {
+        this.idBook = idBook;
+    }
 
-     public String getValue() {
-         return "ID: " + idBook + "\n" +
-                 "Name: " + name + "\n" +
-                 "Author: " + author + "\n" +
-                 "Company: " + company + "\n" +
-                 "Year: " + year + "\n" +
-                 "Pages: " + pages + "\n" +
-                 "Price: " + price;
-     }
+    public int getIdBook() {
+        return idBook;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setValue(int idBook, String name, String author, String company, int year, int pages, int price) {
+        this.idBook = idBook;
+        this.name = name;
+        this.author = author;
+        this.company = company;
+        this.year = year;
+        this.pages = pages;
+        this.price = price;
+    }
+
+    public String getValue() {
+        return "ID: " + idBook + "\n" +
+                "Name: " + name + "\n" +
+                "Author: " + author + "\n" +
+                "Company: " + company + "\n" +
+                "Year: " + year + "\n" +
+                "Pages: " + pages + "\n" +
+                "Price: " + price;
+    }
 
     @Override
     public String toString() {
--- lab2/src/Main.java
+++ lab2/src/Main.java
@@ -1,18 +1,88 @@
 public class Main {
     public static void main(String[] args) {
-        Book[] books = new Book[2];
+        Book[] books = new Book[3];
 
         books[0] = new Book(1, "Clean Code", "Martin", "Book", 2017, 186, 690);
-        books[1] = new Book(2, "Robert", "Second World War", "Book", 2014, 250, 340);
-//        books[2] = new Book(3, "Jhon", "Population", "Book", 2007, 153, 299);
+        books[1] = new Book(2, "Second World War", "Robert", "Book", 2014, 250, 340);
+        books[2] = new Book(3, "Population", "John", "Book", 2007, 153, 299);
 
         System.out.println("All books:\n");
         printBooks(books);
+
+        System.out.println("Books with filter:\n");
+
+        System.out.println("Books which older than 2017:\n");
+        filterYear(books, 2017);
+
+        System.out.println("Books with more than 200 pages:\n");
+        filterPages(books, 200);
+
+        System.out.println("Books name Population:\n");
+        filterName(books, "Population");
+
+        System.out.println("Books whose author is John:\n");
+        filterAuthor(books, "John");
+
+        System.out.println("Company name is Book:\n");
+        filterCompany(books, "Book");
+
+        System.out.println("Books cheaper than 500:\n");
+        filterPrice(books, 500);
+
+
+    }
+
+    private static void filterPages(Book[] books, int filterPages) {
+        for (Book book : books) {
+            if (filterPages < book.getPages()) {
+                System.out.println(book + "\n");
+            }
+        }
+    }
+
+    private static void filterName(Book[] books, String filterName) {
+        for (Book book : books) {
+            if (book.getValue().contains("Name: " + filterName)) {
+                System.out.println(book + "\n");
+            }
+        }
+    }
+
+    private static void filterAuthor(Book[] books, String filterAuthor) {
+        for (Book book : books) {
+            if (book.getValue().contains("Author: " + filterAuthor)) {
+                System.out.println(book + "\n");
+            }
+        }
+    }
+
+    private static void filterCompany(Book[] books, String filterCompany) {
+        for (Book book : books) {
+            if (book.getValue().contains("Company: " + filterCompany)) {
+                System.out.println(book + "\n");
+            }
+        }
+    }
+
+    private static void filterPrice(Book[] books, int filterPrice) {
+        for (Book book : books) {
+            if (filterPrice >= book.getPrice()) {
+                System.out.println(book + "\n");
+            }
+        }
     }
 
     public static void printBooks(Book[] books) {
-        for(int i = 0; i < books.length; i++) {
-            System.out.println(books[i] + "\n");
+        for (Book book : books) {
+            System.out.println(book + "\n");
+        }
+    }
+
+    public static void filterYear(Book[] books, int filterYear) {
+        for (Book book : books) {
+            if (filterYear > book.getYear()) {
+                System.out.println(book + "\n");
+            }
         }
     }
 }
